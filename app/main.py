a28e9e077a90459fb283e76886338ebdbff30577
natalie <natalie>
2024-03-30 22:05:22 +0000

    repositories now find correctly, and catch git errors

--- app/main.py
+++ app/main.py
@@ -1,17 +1,33 @@
-from git import Repo
+from git import Repo, Commit, GitError
 import os
+from datetime import datetime, timedelta
+import time
 
 CODE_REPOSITORY = os.path.abspath("/home/natalie/code/")
 
 def find_repositories(dirpath: str = CODE_REPOSITORY) -> list[Repo]:
     repos: list[Repo] = []
     for child in os.listdir(dirpath):
-        if os.path.isdir(child):
-            repo = Repo(child)
-            #except :
-            repos.append(repo)
+        child_path = os.path.join(dirpath, child)
+        if os.path.isdir(child_path):
+            try:
+                print("isdir" + child)
+                repo = Repo(child_path)
+            except GitError:
+                print(GitError)
+            else:
+                repos.append(repo)
+        else:
+            print("noisdir" + child)
 
     return repos
+
+def get_commits(repo: Repo, commits: int = 1, since: int = 7) -> list[Commit]:
+    print("weee")
+    repo_commits = repo.iter_commits("master", max_count=commits)
+    print(repo_commits)
+
+    return list(repo_commits)
 
 def main():
     repos: list[Repo] = find_repositories()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,13 @@
 import unittest
-from app.main import find_repositories 
+from app.main import find_repositories, get_commits 
 from git import Repo
 
 class TestGitSome(unittest.TestCase):
     def test_can_find_repos(self):
         repositories = find_repositories()
+        print(repositories)
         for repository in repositories:
             self.assertIsInstance(repository, Repo)
+            commits = get_commits(repository)
+            print(commits)
+    
