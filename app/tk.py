fabdd68e7f0c4e15ed1c2364cf5eebf8daa31222
natalie <natalie>
2024-03-30 23:14:35 +0000

    init tk branch

---
+++ app/tk.py
@@ -0,0 +1,5 @@
+import tkinter
+
+def main():
+    print("hello")
+
---
+++ tests/test_tk.py
@@ -0,0 +1,6 @@
+import unittest
+from app.tk import main
+
+class TestTk(unittest.TestCase):
+    def test_main(self):
+        main()
