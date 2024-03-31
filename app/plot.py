2acfa9baa093f89b701ceb597ef213ff6b6a4b64
natalie <natalie>
2024-03-31 21:39:54 +0000

    create basic plot of recent commits. needs commit_is_since bug fixed, which makes it return false in plot branch

--- app/plot.py
+++ app/plot.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 from .main import main
 
 git_data = main()
+
+repos = git_data.keys()
+commits = git_data.values()
+
+plt.figure(figsize=(9,4))
+plt.bar(git_data.keys(), git_data.values())
+plt.show()
