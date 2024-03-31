3bba2b2aa3c08b6f00755357e7a2e90aafb14daa
natalie <natalie>
2024-03-31 01:14:39 +0000

    init plot module

---
+++ app/plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+from .main import main
+
+git_data = main()
---
+++ tests/test_plot.py
@@ -0,0 +1,6 @@
+import unittest
+from app.plot import git_data
+
+class TestPlot(unittest.TestCase):
+    def test_git_data(self):
+        print(git_data)
