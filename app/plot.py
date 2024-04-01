3eb0b30e7ff93d70a539e29e3541debe6c66b6fc
natalie <natalie>
2024-04-01 03:15:04 +0000

    include legend! and legend

--- app/plot.py
+++ app/plot.py
@@ -12,7 +12,7 @@ def plot_repo_by_commit_count():
     plt.bar(repos, commit_count)
     return plt
 
-def plot_repo_commit_history(days: int = 31):
+def plot_repo_commit_history(days: int = 500):
     git_data = main(since=timedelta(days=days))
     repos = list(git_data.keys())
     today = datetime.now()
@@ -29,5 +29,6 @@ def plot_repo_commit_history(days: int = 31):
             repo_commits = [commit for commit in git_data[repo] if not commit_is_since(commit, date_since) ]
             commits_per_date = len(repo_commits)
             commit_history.append(commits_per_date) 
-        plt.plot(dates, commit_history)
+        plt.plot(dates, commit_history, label=repo)
+        plt.legend()
     return plt
