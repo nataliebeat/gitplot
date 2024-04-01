a7011bfcb33f83917b42a32eb9bdcc136dd7e2ed
natalie <natalie>
2024-04-01 01:33:54 +0000

    function creates plot based on data from git-some.main()

--- app/plot.py
+++ app/plot.py
@@ -4,9 +4,10 @@ from .main import main
 
 git_data = main()
 
-repos = git_data.keys()
-commits = git_data.values()
-
-plt.figure(figsize=(9,4))
-plt.bar(git_data.keys(), git_data.values())
-plt.show()
+def plot_repo_by_commit_count():
+    repos = list(git_data.keys())
+    repo_commits = git_data.values()
+    commit_count = [len(commits) for commits in repo_commits]
+    plt.figure(figsize=(9,4))
+    plt.bar(repos, commit_count)
+    return plt
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,6 +1,6 @@
 import unittest
-from app.plot import git_data
+from app.plot import git_data, plot_repo_by_commit_count
 
 class TestPlot(unittest.TestCase):
     def test_git_data(self):
-        print(git_data)
+        plot_repo_by_commit_count()
