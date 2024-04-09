07dbcba271e310bfa6f8abc9d902ab6b42f0f36f
natalie <natalie>
2024-04-09 18:18:49 +0000

    add assertion for fun

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,5 +6,5 @@ from gitsome.cli import parser
 class TestCliArgs(unittest.TestCase):
     def test_repo_directory_path(self):
         args = parser.parse_args(args=['../cribbage'])
-        print(args)
+        self.assertEqual(args.repo_directory_path, ['../cribbage'])
 
