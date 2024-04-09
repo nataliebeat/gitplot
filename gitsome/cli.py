40eb80270803b845126e8814ca195a5609acd91d
natalie <natalie>
2024-04-09 18:25:26 +0000

    now the first argument is the plot type

--- gitsome/cli.py
+++ gitsome/cli.py
@@ -1,5 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Compare histories git repositories.")
-parser.add_argument('-y', '--y-axis')
+parser.add_argument('-y', '--y-unit')
+parser.add_argument('-x', '--x-unit')
+parser.add_argument('-m', '--max_commits')
+parser.add_argument('-s', '--since')
+
+parser.add_argument('plot_type', metavar='plot', type=str, nargs='?', default='line', help='type of plot')
 parser.add_argument('repo_directory_path', metavar='dir', type=str, nargs='+', help='path to git repo')
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,4 +7,9 @@ class TestCliArgs(unittest.TestCase):
     def test_repo_directory_path(self):
         args = parser.parse_args(args=['../cribbage'])
         self.assertEqual(args.repo_directory_path, ['../cribbage'])
+    
+    def test_plot_type(self):
+        args = parser.parse_args(args=['line', '../cribbage', '.'])
+        self.assertEqual(args.plot_type, 'line')
+        self.assertEqual(args.repo_directory_path, ['../cribbage', '.'])
 
