664061f8dfa4fcea1813923988b75f6a91be762e
natalie <natalie>
2024-04-17 16:42:56 +0000

    correct formatting

--- gitplot/cli.py
+++ gitplot/cli.py
@@ -1,10 +1,27 @@
+"""Handles CLI aruments."""
+
 import argparse
 
 parser = argparse.ArgumentParser(description="Compare histories git repositories.")
+
 parser.add_argument('-y', '--y-unit')
+
 parser.add_argument('-x', '--x-unit')
+
 parser.add_argument('-m', '--max_commits')
+
 parser.add_argument('-s', '--since')
 
-parser.add_argument('plot_type', metavar='plot', type=str, nargs='?', default='line', help='type of plot', choices=['line', 'bar'])
-parser.add_argument('repo_directory_path', metavar='dir', type=str, nargs='+', help='path to git repo')
+parser.add_argument('plot_type',
+                    metavar='plot',
+                    type=str,
+                    nargs='?',
+                    default='line',
+                    help='type of plot',
+                    choices=['line', 'bar'])
+
+parser.add_argument('repo_directory_path',
+                    metavar='dir',
+                    type=str,
+                    nargs='+',
+                    help='path to git repo')
