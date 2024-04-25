086bfcb983a6d8c38d5cb72ff8ab5bd139ef0d2c
natalie <natalie>
2024-04-25 16:58:26 +0000

    no longer filters commits by default

--- gitplot/git_tools.py
+++ gitplot/git_tools.py
@@ -1,7 +1,11 @@
 """ Functions using git-python to retrieve data from the listed repositories."""
+import logging
 
 from datetime import datetime, timedelta, timezone
 from git import Repo, Commit, GitError
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 def clean_repo_name(repo: Repo) -> str:
     """Return the last directory in the filepath."""
@@ -20,25 +24,26 @@ def find_repositories(repo_list: list[str]) -> list[Repo]:
             repos.append(repo)
     return repos
 
-def get_commits(repo: Repo, commits: int = 1) -> list[Commit]:
+def get_commits(repo: Repo, commits: int = 0) -> list[Commit]:
     """Given a Git-Python repo, return a list of its commits."""
-    repo_commits = repo.iter_commits(repo.active_branch, max_count=commits)
+    if commits == 0:
+        logger.debug("Getting commits from %s", repo)
+        repo_commits = repo.iter_commits(repo.active_branch)
+    else:
+        repo_commits = repo.iter_commits(repo.active_branch, max_count=commits)
     result = list(repo_commits)
+    logger.debug("Commits from %s: %s", repo, len(result))
     return result
 
 def commit_is_since(commit: Commit, since: timedelta) -> bool:
     """Return True if the commit is since a certain period of time."""
     commit_datetime = commit.committed_datetime
     my_since: timedelta = datetime.now(timezone.utc) - commit_datetime
-    print(my_since)
-    print(since)
     if my_since > since:
-        print('f')
         return False
-    print('t')
     return True
 
-def scan_repos(repo_dirs: list[str], max_commits: int = 1000, since: timedelta = timedelta(days=0)):
+def scan_repos(repo_dirs: list[str], max_commits: int = 0, since: timedelta = timedelta(days=0)):
     """
     Given a list of directories, return a dictionary,
     with all the information needed for plotting.
@@ -49,8 +54,6 @@ def scan_repos(repo_dirs: list[str], max_commits: int = 1000, since: timedelta =
         commits = get_commits(repo, commits = max_commits)
         if since.days != 0:
             commits = [commit for commit in commits if commit_is_since(commit, since)]
-
-        print(repo, commits)
         repo_dict[clean_repo_name(repo)] = commits
 
     return repo_dict
--- gitplot/plot.py
+++ gitplot/plot.py
@@ -1,8 +1,12 @@
 """Plots data from git directories using the .git_tools module and matplotlib"""
+import logging 
 
 from datetime import datetime, timedelta, timezone
 import matplotlib.pyplot as plt
 from .git_tools import scan_repos, commit_is_since
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 def plot_repo_by_commit_count(repo_dirs):
     """Plots amount of commits of given git directories"""
@@ -24,6 +28,7 @@ def plot_repo_commit_history(repo_dirs, days: int = 0):
         earliest_commits = []
         for repo in git_data.items():
             # gets the data of the tuple, then adds last to list of earliest commits
+            logger.debug("Getting earlist commit from: %s", repo)
             earliest_commits.append(repo[1][-1].committed_datetime)
         earliest_commits = [(today - com_date).days for com_date in earliest_commits]
         days = max(earliest_commits)
